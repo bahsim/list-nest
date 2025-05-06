17ae92016843081de761b7c096682aa52df3f45e
Mikhail Burnin <mikhail.burnin@pearson.com>
2025-05-06 13:59:50 +0500

    fixed app naming

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'List Nest - Coming soon.';
+    return 'ListNest - Coming soon.';
   }
 }
