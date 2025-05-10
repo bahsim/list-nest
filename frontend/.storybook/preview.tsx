2959df6a70460949bebdea0e9e302a0d6a2430fd
Mikhail Burnin <mikhail.burnin@pearson.com>
2025-05-10 15:13:37 +0500

    Added MUI Theme to Storybook

---
+++ frontend/.storybook/preview.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import type { Preview } from '@storybook/react'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import listNestTheme from '../src/theme-listnest';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={listNestTheme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
+export default preview;
